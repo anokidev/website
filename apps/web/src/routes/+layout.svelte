468e9e24c4c78e189a9943f8bc172ca97c72442a
anokidev <anoki.youssou@gmail.com>
2023-05-24 22:42:31 +0700

    a little bit of save

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+  import Header from "@website/ui/Header.svelte";
+  import Footer from "@website/ui/Footer.svelte";
+
+</script>
+
+<Header/>
+
--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,15 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
+  
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    target: '#svelte',
+      vite: {
+        resolve: {
+          alias: {
+            $components: resolve('./src/libs/')
+          }
+      }
+    }
   }
+
 };
 
 export default config;
